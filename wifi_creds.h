3b695ce457e6ee55a33afb7cb6e68625a0c7763e
Yves Jeanrenaud <yjeanrenaud@users.noreply.github.com>
2023-11-01 19:00:07 +0100

    Create wifi_creds.h
    initial commit

---
+++ wifi_creds.h
@@ -0,0 +1,3 @@
+/* Put your SSID & Password */
+const char* ssid = "Some SSID";  // Enter SSID here. ESP32-WROOM only supports 2.4 GHz signals
+const char* pass = "some password in plain text"; //Enter Password here
